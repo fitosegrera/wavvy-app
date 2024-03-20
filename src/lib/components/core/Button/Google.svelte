97bf5e5d734d1014e52dc5556e54d9dc2779dbd8
fitosegrera <fitosegrera@users.noreply.github.com>
2024-03-20 23:33:29 +0000

    deploy: wavvy-sup/wavvy-app@c2f8582b0e66487b58ccf59aae53be07ce476884

--- src/lib/components/core/Button/Google.svelte
+++ src/lib/components/core/Button/Google.svelte
@@ -5,34 +5,49 @@
 	import { legalScreenStore } from '$lib/store/overlays/legal';
 	import { classNames } from '$lib/utils/classNames';
 	import Icon from '@iconify/svelte';
-	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+	import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+	import firebase from 'firebase/compat/app';
 
 	const provider = new GoogleAuthProvider();
 	export let disabled: boolean = false;
 
-	const handleGoogleSignIn = () => {
-		signInWithPopup(auth, provider)
-			.then((result) => {
+	const handleGoogleSignIn = (cb: (token: string) => void) => {
+		var provider = new GoogleAuthProvider();
+		signInWithRedirect(auth, provider)
+			.then(function (result) {
+				console.log('Google signin successful', result);
 				// This gives you a Google Access Token. You can use it to access the Google API.
-				const credential = GoogleAuthProvider.credentialFromResult(result);
-				const token = credential?.accessToken;
-				// The signed-in user info.
-				const user = result.user;
-				// IdP data available using getAdditionalUserInfo(result)
-				// ...
-				$isLoading = false;
+				// var token = result.credential.accessToken;
+
+				// Send user  to rest of program
+				const token = '';
+				cb(token);
 			})
-			.catch((error) => {
-				// Handle Errors here.
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				// The email of the user's account used.
-				const email = error.customData.email;
-				// The AuthCredential type that was used.
-				const credential = GoogleAuthProvider.credentialFromError(error);
-				// ...
-				$isLoading = false;
+			.catch(function (error) {
+				console.error(error);
 			});
+		// signInWithPopup(auth, provider)
+		// 	.then((result) => {
+		// 		// This gives you a Google Access Token. You can use it to access the Google API.
+		// 		const credential = GoogleAuthProvider.credentialFromResult(result);
+		// 		const token = credential?.accessToken;
+		// 		// The signed-in user info.
+		// 		const user = result.user;
+		// 		// IdP data available using getAdditionalUserInfo(result)
+		// 		// ...
+		// 		$isLoading = false;
+		// 	})
+		// 	.catch((error) => {
+		// 		// Handle Errors here.
+		// 		const errorCode = error.code;
+		// 		const errorMessage = error.message;
+		// 		// The email of the user's account used.
+		// 		const email = error.customData.email;
+		// 		// The AuthCredential type that was used.
+		// 		const credential = GoogleAuthProvider.credentialFromError(error);
+		// 		// ...
+		// 		$isLoading = false;
+		// 	});
 	};
 
 	const handleLegalScreen = () => {
@@ -45,7 +60,7 @@
 			onConfirm: () => {
 				$legalScreenStore.open = false;
 				$isLoading = true;
-				handleGoogleSignIn();
+				handleGoogleSignIn((token: string) => {});
 			}
 		};
 	};
